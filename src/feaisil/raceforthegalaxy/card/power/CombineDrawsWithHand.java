c11ad0e0bce743172bc14dd86b72c072221d4e9f
Paris MEULEMAN <paris.meuleman@gmail.com>
2013-06-07 08:38:03 +0200

    Develop and explore phases

---
+++ src/feaisil/raceforthegalaxy/card/power/CombineDrawsWithHand.java
@@ -0,0 +1,10 @@
+package feaisil.raceforthegalaxy.card.power;
+
+public class CombineDrawsWithHand extends Power {
+
+  @Override
+  public String toString()
+  {
+    return "CombineDrawsWithHand []";
+  }
+}
